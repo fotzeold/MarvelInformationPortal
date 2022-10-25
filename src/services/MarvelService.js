d332912ee2aec8139cd38400af5e6f29ac15f4a7
Roman Popovych <mechenuyzzzz@gmail.com>
2022-10-25 14:42:58 +0300

    visual part + component with random character

---
+++ src/services/MarvelService.js
@@ -0,0 +1,36 @@
+class MarvelService {
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=3cc8cb42144fbc2d62270ab60dd9bced'
+
+	getResource = async (url) => {
+		let result = await fetch(url);
+
+		if (!result.ok) {
+			throw new Error(result.status);
+		}
+
+		return await result.json();
+	}
+
+	getAllCharacters = async () => {
+		const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+		return res.data.results.map(this._transformCharacter)
+	}
+
+	getCharacter = async (id) => {
+		const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+		return this._transformCharacter(res.data.results[0]);
+	}
+
+	_transformCharacter = (char) => {
+		return {
+			name: char.name,
+			description: char.description ? char.description : 'There is no information about this character',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+		}
+	}
+}
+
+export default MarvelService;
